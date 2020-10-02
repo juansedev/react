c1380e68d8991feeddc380d6aaa01be255b8fb5b
juanllano93@gmail.com <juanllano93@gmail.com>
2020-10-02 10:32:25 -0500

    add componentes and react hooks

---
+++ dashboard_react/src/components/App.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import "./globals.css";
+import Header from "./header/header.js";
+import TopCardList from "./top-card-list/top-card-list.js";
+import Overview from "./overview/overview.js";
+import Switch from "./switch/switch.js";
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [checked, setChecked] = useState(false);
+  const mainClass = darkMode ? "is-dark-mode" : "is-light-mode";
+
+  function changeMedia(mq) {
+    setDarkMode(mq.matches);
+    setChecked(mq.matches);
+  }
+
+  useEffect(() => {
+    const mq = window.matchMedia("(prefers-color-scheme: dark)");
+    mq.addListener(changeMedia);
+    setDarkMode(mq.matches);
+    setChecked(mq.matches);
+    return () => {
+      mq.removeListener(changeMedia);
+    };
+  }, []);
+
+  return (
+    <main className={mainClass}>
+      <Header>
+        <Switch
+          setDarkMode={setDarkMode}
+          checked={checked}
+          setChecked={setChecked}
+        />
+      </Header>
+      <TopCardList />
+      <Overview />
+    </main>
+  );
+}
+export default App;
